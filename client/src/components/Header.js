d30b916b977393c83d749885c8a71bd81ae32a0b
Matthew Swineford <m.swineford@yahoo.com>
2023-01-10 18:05:51 -0500

    fixed react to be similar to the way I learned it

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+    return (
+        <header>
+            <h2 className="header">
+                Matthew Swineford
+            </h2>
+        </header>
+    );
+}
+
+export default Header;
